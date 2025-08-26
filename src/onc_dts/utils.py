6397c0f2275b82e9f054942c549abf9a5d1d6d00
aschlesin <schlesin.angela@gmail.com>
2025-08-26 16:04:02 -0700

    changed utils.py to add switch between ONC JSON and instrument xt JSON

--- src/onc_dts/utils.py
+++ src/onc_dts/utils.py
@@ -17,6 +17,7 @@ import matplotlib.pyplot as plt
 def read_xt_file(file_path: Union[str, Path], 
                  include_raw: bool = False,
                  trim: bool = True, 
+                 json: bool = True,
                  channel_points: Dict[int, int] = {1: 2206, 2: 1561}) -> Dict[str, Any]:
     """Reads and parses a .xt file, extracting temperature data and metadata.
     
@@ -26,6 +27,8 @@ def read_xt_file(file_path: Union[str, Path],
     Args:
         file_path: Path to the .xt file as string or Path object
         include_raw: Whether to include raw signal data in the output
+        json: This switches between JSON formatted ONC files and JSON xt files downloaded
+              directly from the instrument.
         channel_points: Dictionary mapping channel numbers to the number of points
                         to extract for that channel
         
@@ -47,6 +50,10 @@ def read_xt_file(file_path: Union[str, Path],
     with open(file_path, 'r') as f:
         json_data = json.load(f)
     
+    # Determine if ONC JSON formatted or instrument downloaded JSON
+    if json:
+        json_data = json_data['Resp']
+
     out = {}
     # Extract metadata from the JSON file
     metadata = {
