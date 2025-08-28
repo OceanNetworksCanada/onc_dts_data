d6e3143962c8e2951e99843334cfedc46be29ae7
Martin Heesemann <mheesema@uvic.ca>
2025-08-27 20:20:05 -0700

    Add working directory logging in monitor_dts script

--- src/onc_dts/monitor_dts.py
+++ src/onc_dts/monitor_dts.py
@@ -9,6 +9,9 @@ ONC_API_TOKEN = os.getenv("ONC_API_TOKEN")
 if not ONC_API_TOKEN:
     raise EnvironmentError("ONC_API_TOKEN not found in environment variables.")
 
+working_dir = os.getcwd()
+print(f"Working directory: {working_dir}")
+
 def main() -> None:
     print("Monitoring DTS...")
     parser = argparse.ArgumentParser(description="Monitor DTS with start time.")
