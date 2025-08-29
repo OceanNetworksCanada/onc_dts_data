cae68da5ecb0fb5b63f251941d64596576da1a59
Martin Heesemann <mheesema@uvic.ca>
2025-08-29 16:48:53 -0700

    Refactor logging in monitor_dts script to replace print statements with logger calls and improve environment variable loading

--- src/onc_dts/monitor_dts.py
+++ src/onc_dts/monitor_dts.py
@@ -1,24 +1,30 @@
 import argparse
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 import os
+import logging
 
-load_dotenv()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+env_file = find_dotenv(usecwd=True)
+logger.info(f"Loading .env file from: {env_file}")
+out = load_dotenv(env_file, verbose=True)
+logger.info(f".env loaded: {out}")
 
 ONC_API_TOKEN = os.getenv("ONC_API_TOKEN")
 
 working_dir = os.getcwd()
-print(f"Working directory: {working_dir}")
+logger.info(f"Working directory: {working_dir}")
 
 if not ONC_API_TOKEN:
-    # raise EnvironmentError("ONC_API_TOKEN not found in environment variables.")
-    print(f"Warning: ONC_API_TOKEN not found in environment variables. {ONC_API_TOKEN}")
+    logger.warning(f"ONC_API_TOKEN not found in environment variables. {ONC_API_TOKEN}")
 
 def main() -> None:
-    print("Monitoring DTS...")
+    logger.info("Monitoring DTS...")
     parser = argparse.ArgumentParser(description="Monitor DTS with start time.")
     parser.add_argument('--start-time', type=str, required=True, help='Start time for monitoring (e.g., "2024-06-01T12:00:00")')
     args = parser.parse_args()
 
     start_time = args.start_time
-    print(f"Monitoring DTS from start time: {start_time}")
+    logger.info(f"Monitoring DTS from start time: {start_time}")
     # Add monitoring logic here
